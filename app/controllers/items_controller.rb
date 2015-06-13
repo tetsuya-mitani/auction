24c7fa0bd126389f9975e0582cb83eda1efc1ef3
三谷 哲也 <mitani0328@hotmail.co.jp>
2015-06-13 18:20:26 +0900

    itemの新規追加を実装。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,30 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    # formから投げられたデータを受け取る
+    @item = Item.new(item_params)
+    # これを保存する
+    @item.save
+    # show.html.erbに飛ばす
+    redirect_to "/items/#{@item.id}"
+  end
+
+  private
+  def item_params
+    # params.require(:key).permit(:filter)
+    params.require(:item).permit(
+      :name,
+      :description,
+      :price,
+      :seller_id,
+      :email,
+      :image_url
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,11 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  get 'items/new' => 'items#new'
   get 'items/:id' => 'items#show'
   get 'items' => 'items#index'
+  post 'items' => 'items#create'
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
